2ad486c7ed8f12e2ede0200ca931d4cc6c2280f2
twer4774 <twer4774@gmail.com>
2016-04-06 23:50:26 +0900

    Round07

--- JavaProgramming/src/Round04InputOutput/Ex06Exception2.java
+++ JavaProgramming/src/Round04InputOutput/Ex06Exception2.java
@@ -8,7 +8,7 @@ import java.io.IOException;
 public class Ex06Exception2 {
     public static void  main(String[] ar) throws IOException {
         System.out.print("입력= ");
-        int aa = System.in.read();
+        int aa = System.in.read(); //System.in.read()는 아스키코드값으로 입력된다.
         System.out.println("입력된 아스키 값은 " + aa + "입니다.");
     }
 }
